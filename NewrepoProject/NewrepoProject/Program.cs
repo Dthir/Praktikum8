b1cb03fec98346621915855dd52161ce22ba724e
Melvin Immanuel <melvinimanuel7@gmail.com>
2021-12-08 23:21:58 +0900

    Commit ke Branch baru

--- NewrepoProject/NewrepoProject/Program.cs
+++ NewrepoProject/NewrepoProject/Program.cs
@@ -8,6 +8,7 @@ namespace NewrepoProject
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Pemrogaman Lanjut S1 Informatika");
+            Console.WriteLine("Materi Praktikum Git");
         }
     }
 }
