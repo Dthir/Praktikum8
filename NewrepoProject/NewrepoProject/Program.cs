90ff7d61bf265163205d73c7f189dc229821053b
Melvin Immanuel <melvinimanuel7@gmail.com>
2021-12-08 22:48:30 +0900

    ini initial commit

---
+++ NewrepoProject/NewrepoProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewrepoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
